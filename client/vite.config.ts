948606d5a5d3cd733e08b6b879cd51b4fbf4a8bd
umerfarooq777 <umerfarooq03100276796@gmail.com>
2024-05-06 22:35:51 +0500

    minor changes config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import nodePolyfills from 'vite-plugin-node-stdlib-browser';
 // Define a type for the environment variables object
 type ProcessEnv = {
   [key: string]: string;
@@ -24,6 +25,6 @@ export default defineConfig(({ mode }) => {
       global: {},
       'process.env': processEnv
     },
-    plugins: [react()],
+    plugins: [react(), nodePolyfills()],
   }
 })
