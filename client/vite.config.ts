85ed6bf0c4b5940fa34229dd879b669c0563709a
Shine Li <shine.li@immutable.com>
2024-05-09 20:25:37 +1000

    make it work

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import nodePolyfills from 'vite-plugin-node-stdlib-browser';
+import { resolve } from 'path';
 // Define a type for the environment variables object
 type ProcessEnv = {
   [key: string]: string;
@@ -14,10 +15,10 @@ type ProcessEnv = {
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  
+
   // Initialize processEnv with the ProcessEnv type
   const processEnv: ProcessEnv = {};
-  
+
   // cherryPickedKeys.forEach(key => processEnv[key] = env[key]);
 
   return {
@@ -26,5 +27,10 @@ export default defineConfig(({ mode }) => {
       'process.env': processEnv
     },
     plugins: [react(), nodePolyfills()],
+    resolve: {
+      alias: {
+        jsbi: resolve(__dirname, '.', 'node_modules', 'jsbi', 'dist', 'jsbi-cjs.js'),
+      },
+    },
   }
 })
